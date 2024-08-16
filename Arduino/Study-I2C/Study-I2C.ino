4fe9a518f24d8afb06e377ebcb5c91b6b77674ca
GOROman <yoshihito.kondoh@gmail.com>
2024-08-16 13:33:26 +0900

    Arduino: I2Cライブラリ Wire を追加し、I2Cを初期化、loop()でI2Cスレーブに1バイト書き込む

--- Arduino/Study-I2C/Study-I2C.ino
+++ Arduino/Study-I2C/Study-I2C.ino
@@ -1,10 +1,29 @@
+// I2C マスターのサンプル
+
+// I2C Wireライブラリ
+#include <Wire.h>
+
+#define I2C_ADDR 0x45
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.printf("Hello I2C!");
+
+  // I2C初期化
+  Wire.begin(); 
 }
 
 void loop() {
+  uint8_t err = 0;
   // put your main code here, to run repeatedly:
-
+  Wire.beginTransmission(I2C_ADDR);
+  int sentBytes = Wire.write(0x10);
+  err = Wire.endTransmission();
+  if ( err != 0 ) {
+    Serial.printf("I2C: Error endTransmission = %d\n", err );
+  } else {
+    Serial.printf("I2C: OK! %dbytes\n", sentBytes);
+  }
+  delay(500);
 }
