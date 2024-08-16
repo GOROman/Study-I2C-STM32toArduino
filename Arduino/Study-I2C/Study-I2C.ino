2b60f5f59a9bf0ed02d5038b806f6b29f039b107
GOROman <yoshihito.kondoh@gmail.com>
2024-08-16 22:21:49 +0900

    STM32 I2C_DUALADDRESS と I2C_GENERALCALL を有効に

--- Arduino/Study-I2C/Study-I2C.ino
+++ Arduino/Study-I2C/Study-I2C.ino
@@ -3,7 +3,9 @@
 // I2C Wireライブラリ
 #include <Wire.h>
 
-#define I2C_ADDR 0x45
+#define I2C_PRIMARY_ADDR 0x45
+#define I2C_SECONDARY_ADDR 0x46
+#define I2C_GENERAL_CALL_ADDR 0x00
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,48 +16,83 @@ void setup() {
   Wire.begin(); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  uint8_t data[] = {0}; // 先頭のバイトが残りのバイト数を意味する
-
-  Wire.beginTransmission(I2C_ADDR);
+void writeI2C(uint8_t addr, uint8_t* data, int size)
+{
+  Serial.printf("I2C: beginTransmission(0x%02x)\n", addr);
+  Wire.beginTransmission(addr);
 
   int sentBytes = 0;
 
-  for (int i=0; i<sizeof(data); ++i ) {
-    sentBytes += Wire.write(data[i]);
+  for (int i=0; i<size; ++i ) {
+    int res = Wire.write(data[i]);
+    sentBytes += res;
+    Serial.printf("I2C: [%d] Wire.write(0x%02x) -> %d\n", i, data[i], res);  
   }
 
   uint8_t err = Wire.endTransmission(false);
   switch(err) {
     case 0:
-      Serial.printf("I2C: OK! %dbytes\n", sentBytes);
+      Serial.printf("I2C: Wire.endTransmission %dbytes\n", sentBytes);
     break;
     case 1:
-      Serial.printf("I2C: Error(%d) data too long to fit in transmit buffer.\n", err );
+      Serial.printf("I2C: Wire.endTransmission Error(%d) data too long to fit in transmit buffer.\n", err );
     break;
     case 2:
-      Serial.printf("I2C: Error(%d) received NACK on transmit of address.\n", err );
+      Serial.printf("I2C: Wire.endTransmission Error(%d) received NACK on transmit of address.\n", err );
     break;
     case 3:
-      Serial.printf("I2C: Error(%d) received NACK on transmit of data.\n", err );
+      Serial.printf("I2C: Wire.endTransmission Error(%d) received NACK on transmit of data.\n", err );
     break;
     case 4:
-      Serial.printf("I2C: Error(%d) other error.\n", err );
+      Serial.printf("I2C: Wire.endTransmission Error(%d) other error.\n", err );
     break;
     case 5:
-      Serial.printf("I2C: Error(%d) timeout.\n", err );
+      Serial.printf("I2C: Wire.endTransmission Error(%d) timeout.\n", err );
     break;
     default:
-      Serial.printf("I2C: Error(%d) unknown error.\n", err );
+      Serial.printf("I2C: Wire.endTransmission Error(%d) unknown error.\n", err );
   }      
-  delay(500);
-  int length = Wire.requestFrom(I2C_ADDR, 3);
-  Serial.printf("Wire.requestFrom() = %d\n", length);
+}
 
-  for (int i=0;i<length;++i) {
+int readI2C(uint8_t addr, uint8_t* data, int len)
+{
+  int result = Wire.requestFrom(addr, len);
+  Serial.printf("I2C: Wire.requestFrom(0x%02x, %d) = %d\n", addr, len, result);
+
+  for (int i=0;i<len;++i) {
     byte data = Wire.read();
-    Serial.printf(">>[%d] -> %02x\n", i, data);
+    Serial.printf("I2C: [%d] Wire.read() -> 0x%02x\n", i, data);
   }
+  return result;
+}
+
+void test(uint8_t addr)
+{
+  uint8_t data[] = {1,2,3};
+  Serial.printf("=============================== I2C ADDR: 0x%02x\n", addr );
+  writeI2C(addr, data,sizeof(data));
+
   delay(500);
+
+  int len = 3;
+  byte buffer[3];
+  readI2C(addr, buffer, len);
+
+  delay(500);
+
+}
+
+void loop() {
+  // STM32 I2C Dual Addressのテスト
+  {
+    test(I2C_PRIMARY_ADDR); // Primary
+    test(I2C_SECONDARY_ADDR); // Secondary
+  }
+  
+  // STM32 I2C General Call Addressのテスト(送信のみ)
+  {
+    Serial.printf("=============================== General Call Address\n" );
+    byte data[] = {0xff};
+    writeI2C(0x00, data, sizeof(data));    
+  }
 }
